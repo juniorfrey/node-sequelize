84605976d180e282d7fc5afae979220b4d18da09
juniorfrey <48813208+juniorfrey@users.noreply.github.com>
2022-10-13 21:53:49 +0000

    Se creo ejemplo con express, se creo servidor, y se genero una ruta de tipo get.

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
- var http = require('http');
- var colors = require('colors');
+ 
+ const express = require('express');
+
+ var app = express();
+
+ //MEOTODOS DE SOLICITUDES HTTP
+ //Peticion get
+ app.get('/', (pet, res) => {
+   res.send('ruta de inicio')
+ });
+
+ app.get('/contacto', (pet, res) => {
+    res.send('ruta de contacto')
+  });
+
+ app.listen(5000, (pet, res) => {
+    console.log('Hola mundo');
+ });
+ 
+ /* var http = require('http');
+ var colors = require('colors'); */
 
  // crear servidor
- var servidor = http.createServer( (peticion, respuesta) => {
+ /* var servidor = http.createServer( (peticion, respuesta) => {
     respuesta.writeHead(200, {'Content-type':'text/html;charset=utf-8'});
     respuesta.write('<h3>Server Básico con NODE JS</h3>');
     console.log('Petición WEB');
@@ -10,7 +29,7 @@
  });
 
  servidor.listen(5000);
- console.log('Ejecuntando servidor');
+ console.log('Ejecuntando servidor'); */
 
 /*  var prueba = require('./funciones/funcion');
 prueba.suma(3,6);
