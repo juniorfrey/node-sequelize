8ca7687983f33bf06698c1fecf65cc5e5350172a
juniorfrey <48813208+juniorfrey@users.noreply.github.com>
2022-10-13 21:38:14 +0000

    Se instalo la libreria "NODEMON" para ejecutar la aplicación de manera automatica cada vez que se guarde un cambio.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+ var http = require('http');
  var colors = require('colors');
+
+ // crear servidor
+ var servidor = http.createServer( (peticion, respuesta) => {
+    respuesta.writeHead(200, {'Content-type':'text/html;charset=utf-8'});
+    respuesta.write('<h3>Server Básico con NODE JS</h3>');
+    console.log('Petición WEB');
+    respuesta.end();
+ });
+
+ servidor.listen(5000);
+ console.log('Ejecuntando servidor');
 
 /*  var prueba = require('./funciones/funcion');
 prueba.suma(3,6);
